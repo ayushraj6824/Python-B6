3397e6cd36aa96ab342ce43ed3bcdd2bd24e00e7
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-09-29 10:39:20 +0530

    Added request libaray......

--- Request_Lib/request.py
+++ Request_Lib/request.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def download_file(url, file_name):
     # Send a GET request to the provided URL
     response = requests.get(url)
@@ -11,11 +12,12 @@ def download_file(url, file_name):
             file.write(response.content)
         print(f"File '{file_name}' downloaded successfully.")
     else:
-        print(f"Failed to download file. Status code: {response.status_code}")
+        print(f"Failed to download file. Status code: {response.status_code}") #Example usage
 
-# Example usage
+
 if __name__ == "__main__":
-    url = 'https://th.bing.com/th/id/OIP.U_VJuupQohwnzXcKMztqWgHaEo?rs=1&pid=ImgDetMain'
-    file_name = 'img.jpg'
-    download_file(url, file_name)
+	url = 'https://th.bing.com/th/id/R.683f53bffd6b4f7086732a203dbd7204?rik=DXCsbF4Vc6O4lg&riu=http%3a%2f%2f1.bp.blogspot.com%2f-_fvkqH0Yu8I%2fTndklja9_OI%2fAAAAAAAAPGU%2fSdr4QPgK8D4%2fs1600%2fnaruto-shippuden-hd-wallpapers_06.jpg&ehk=M7BuDH6ZN06DasbUkaIdxCeQpy9HrYGTxVWc5pquLKk%3d&risl=&pid=ImgRaw&r=0'
+	file_name = 'img.jpg'
+	download_file(url, file_name)
+
 
